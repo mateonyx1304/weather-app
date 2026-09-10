81666a2dc46c350b3beab29cac87b9da97449219
Mateo Angel <mateonyx1304@gmail.com>
2026-09-10 11:50:48 -0500

    finalización de estilos, selección de elementos del DOM

---
+++ src/dom.js
@@ -0,0 +1,28 @@
+// SELECCIÓN DE ELEMENTOS DEL DOM
+
+// BÚSQUEDA
+
+export const inputCiudad = document.getElementById('inputCiudad');
+export const btnBuscar = document.getElementById('btnBuscar');
+
+
+// CLIMA PRINCIPAL
+
+export const iconoClima = document.getElementById('iconoClima');
+export const temperatura = document.getElementById('temperatura');
+export const ciudad = document.getElementById('ciudad');
+export const descripcion = document.getElementById('descripcion');
+
+
+// DETALLES
+
+export const sensacion = document.getElementById('sensacion');
+export const humedad = document.getElementById('humedad');
+export const viento = document.getElementById('viento');
+export const presion = document.getElementById('presion');
+export const visibilidad = document.getElementById('visibilidad');
+
+
+// PRONÓSTICO
+
+export const contenedorPronostico = document.querySelector('.pronostico');
